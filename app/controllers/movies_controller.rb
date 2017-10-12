92a00f158643d21c6b296bc068fd3198769a5606
Marco Mercado <mercadomax15@gmail.com>
2017-10-12 04:49:10 +0000

    End of Section 4.7 along with some fixed bugs

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,26 +1,29 @@
-# This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
   def index
     @movies = Movie.all
   end
-  
-  private
 
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
+private
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
-  
-  def show
-  id = params[:id] # retrieve movie ID from URI route
-  @movie = Movie.find(id) # look up movie by unique ID
-  # will render app/views/movies/show.html.haml by default
-  end
-  
-  def new
-  @movie = Movie.new
-  # default: render 'new' template
-  end 
-  
-  
 end
